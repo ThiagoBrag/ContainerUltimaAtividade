e724363a93809f17db7f127492633cd5ed8559ce
viniciu <92130098+ThiagoBrag@users.noreply.github.com>
2022-02-21 21:26:43 -0300

    a
    a

--- gerencia-ja/src/app/modulo-resto/cadstro-pedidos/cadstro-pedidos.component.ts
+++ gerencia-ja/src/app/modulo-resto/cadstro-pedidos/cadstro-pedidos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service'
 
 @Component({
   selector: 'app-cadstro-pedidos',
@@ -16,7 +17,8 @@ export class CadstroPedidosComponent implements OnInit {
   status: 'ABERTO';
   constructor(
     public router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private usuarioService: UsuarioService
   ) {
     const id = this.route.snapshot.paramMap.get('id');
     this.id = id;
@@ -36,24 +38,53 @@ export class CadstroPedidosComponent implements OnInit {
 
 
   ngOnInit() {
+    this.usuarioService.buscarCliente()
+    .then((resultado: User[]) => {
+       console.log(resultado)
+      for (let i = 0; i < resultado.length; i++) {
+        let info = {
+          nome: resultado[i].NOME,
+          surname: resultado[i].SOBRENOME
+        }
+        this.clientes.push(info)
+      }
+    })
+
+    this.usuarioService.buscarProduto()
+    .then((resultado: Produto[]) => {
+       console.log(resultado)
+      for (let i = 0; i < resultado.length; i++) {
+        let info = {
+          nome: resultado[i].NOME,
+        }
+        this.produtos.push(info)
+      }
+    })
   }
 
   cadastrar() {
+    if (this.clienteId && this.listaProdutosId) {
+      const pedido = { clienteId: this.clienteId, listaProdutosId: this.listaProdutosId}
 
-    
-    // if (this.clienteId && this.listaProdutosId) {
-    //   const pedido = { clienteId: this.clienteId, listaProdutosId: this.listaProdutosId}
-
-    //   if (this.id == 'novo') {
-    //     this.pedidos.push(pedido);
-    //   } else {
-    //     this.pedidos[this.id] = pedido;
-    //   }
-    //   localStorage.setItem('PEDIDOS', JSON.stringify(this.pedidos));
-    //   this.router.navigate(['/pedidos']);
-    // } else {
-    //   alert('É necessário preencher todos os campos!');
-    // }
+      if (this.id == 'novo') {
+        this.pedidos.push(pedido);
+      } else {
+        this.pedidos[this.id] = pedido;
+      }
+      localStorage.setItem('PEDIDOS', JSON.stringify(this.pedidos));
+      this.router.navigate(['/pedidos']);
+    } else {
+      alert('É necessário preencher todos os campos!');
+    }
   }
 
+}
+
+interface User {
+  NOME: string;
+  SOBRENOME: string
+}
+
+interface Produto {
+  NOME: string;
 }
