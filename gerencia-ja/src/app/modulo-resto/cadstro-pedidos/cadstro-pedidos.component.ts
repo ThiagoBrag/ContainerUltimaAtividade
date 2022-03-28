0b419184f5175b564b63396e2da0cb9364ae3f44
viniciu <92130098+ThiagoBrag@users.noreply.github.com>
2022-03-28 19:40:48 -0300

    a

--- gerencia-ja/src/app/modulo-resto/cadstro-pedidos/cadstro-pedidos.component.ts
+++ gerencia-ja/src/app/modulo-resto/cadstro-pedidos/cadstro-pedidos.component.ts
@@ -25,7 +25,7 @@ export class CadstroPedidosComponent implements OnInit {
   clientes = [];
   clienteId = undefined;
   produtoId = undefined;
-  listaProdutosId = [];
+  ProdutoId = undefined;
   status: 'ABERTO';
   nomePessoa = ''
   nomeProduto = ''
@@ -46,7 +46,7 @@ export class CadstroPedidosComponent implements OnInit {
 
       if (this.pedidos[this.id]) {
         this.clienteId = this.pedidos[this.id].clienteId;
-        this.listaProdutosId = this.pedidos[this.id].listaProdutosId;
+        this.ProdutoId = this.pedidos[this.id].listaProdutosId;
       }
     }
   }
@@ -54,24 +54,19 @@ export class CadstroPedidosComponent implements OnInit {
 
   ngOnInit() {
 
-    // NÃO PEGA OS IDS DO INDEX QUE CLIQUEI CASO EU EXCLUA UM PEDIDO
-
     if (this.id != 'novo') {
       this.index = this.router.url.substring(this.router.url.length - 1);
-      console.log(this.index)
       this.usuarioService.buscarPedido()
         .then((resultado: any) => {
-          resultado.find(valorPedido => {
-            console.log(valorPedido)
-            if (valorPedido.ID == this.index) {
-              console.log("ID DO PEDIDO", valorPedido.ID)
-              this.clienteId = valorPedido.CLIENTE_ID;
-              this.listaProdutosId = valorPedido.PRODUTO_ID;
-              this.enderecoId = valorPedido.ENDERECO_ID;
-              console.log("CLIENTE ID ",this.clienteId, "PRODUTO_ID ", this.listaProdutosId, "ENDERECO_ID ", this.enderecoId)
+          resultado.find(valorBusca => {
+            if (valorBusca.ID == this.index) {
+              this.clienteId = valorBusca.CLIENTE_ID;
+              this.ProdutoId = valorBusca.PRODUTO_ID;
+              this.enderecoId = valorBusca.ENDERECO_ID
+              console.log("ID CLIENTE ", valorBusca.CLIENTE_ID, "ID PRODUTO ", valorBusca.PRODUTO_ID, "ID ENDERECO ", valorBusca.ENDERECO_ID)
+
               this.usuarioService.buscarEndereco().then((resultado: any) => {
                 resultado.find(valorEndereco => {
-
                   if (valorEndereco.ID == this.enderecoId) {
 
                     this.pais = valorEndereco.PAIS;
@@ -82,12 +77,15 @@ export class CadstroPedidosComponent implements OnInit {
                     this.numero = valorEndereco.NUMERO;
                     localStorage.setItem('NUMEROANTIGO', this.numero);
                     this.cep = valorEndereco.CEP;
+                    localStorage.setItem('CEPANTIGO', this.cep)
+
                   }
                 })
               })
             }
           })
         })
+
     }
 
     this.usuarioService.buscarCliente()
@@ -110,20 +108,76 @@ export class CadstroPedidosComponent implements OnInit {
           this.produtos.push(info)
         }
       })
+
+    // NÃO PEGA OS IDS DO INDEX QUE CLIQUEI CASO EU EXCLUA UM PEDIDO
+
+    // if (this.id != 'novo') {
+    //   this.index = this.router.url.substring(this.router.url.length - 1);
+    //   console.log(this.index)
+    //   this.usuarioService.buscarPedido()
+    //     .then((resultado: any) => {
+    //       resultado.find(valorPedido => {
+    //         console.log(valorPedido)
+    //         if (valorPedido.ID == this.index) {
+    //           console.log("ID DO PEDIDO", valorPedido.ID)
+    //           this.clienteId = valorPedido.CLIENTE_ID;
+    //           this.listaProdutosId = valorPedido.PRODUTO_ID;
+    //           this.enderecoId = valorPedido.ENDERECO_ID;
+    //           console.log("CLIENTE ID ",this.clienteId, "PRODUTO_ID ", this.listaProdutosId, "ENDERECO_ID ", this.enderecoId)
+    //           this.usuarioService.buscarEndereco().then((resultado: any) => {
+    //             resultado.find(valorEndereco => {
+
+    //               if (valorEndereco.ID == this.enderecoId) {
+
+    //                 this.pais = valorEndereco.PAIS;
+    //                 this.estado = valorEndereco.ESTADO;
+    //                 this.cidade = valorEndereco.CIDADE;
+    //                 this.bairro = valorEndereco.BAIRRO;
+    //                 this.rua = valorEndereco.RUA;
+    //                 this.numero = valorEndereco.NUMERO;
+    //                 localStorage.setItem('NUMEROANTIGO', this.numero);
+    //                 this.cep = valorEndereco.CEP;
+    //               }
+    //             })
+    //           })
+    //         }
+    //       })
+    //     })
+    // }
+
+    // this.usuarioService.buscarCliente()
+    //   .then((resultado: any) => {
+    //     for (let i = 0; i < resultado.length; i++) {
+    //       let info = {
+    //         nome: resultado[i].NOME,
+    //         surname: resultado[i].SOBRENOME
+    //       }
+    //       this.clientes.push(info)
+    //     }
+    //   })
+
+    // this.usuarioService.buscarProduto()
+    //   .then((resultado: any) => {
+    //     for (let i = 0; i < resultado.length; i++) {
+    //       let info = {
+    //         nome: resultado[i].NOME,
+    //       }
+    //       this.produtos.push(info)
+    //     }
+    //   })
   }
 
   cadastrar() {
-    if (this.clienteId && this.listaProdutosId && this.pais && this.estado && this.cidade && this.bairro && this.rua && this.numero && this.cep) {
-      
+
+    if (this.clienteId && this.ProdutoId && this.pais && this.estado && this.cidade && this.bairro && this.rua && this.numero && this.cep) {
+
       this.usuarioService.inserirEndereco(this.pais, this.estado, this.cidade, this.bairro, this.rua, this.numero, this.cep)
       this.usuarioService.buscarEndereco().then((resultado: any) => {
         for (let i = 0; i < resultado.length; i++) {
           if (this.numero == resultado[i].NUMERO && this.cep == resultado[i].CEP) {
-            console.log("ENDEREÇO ID: ",resultado[i].ID)
-            console.log("CLIENTE ID: ", this.clienteId)
-            console.log("PRODUTO ID: ", this.listaProdutosId)
+
             this.enderecoId = resultado[i].ID;
-            this.usuarioService.inserirPedido(this.clienteId, this.listaProdutosId, this.enderecoId)
+            this.usuarioService.inserirPedido(this.clienteId, this.ProdutoId, this.enderecoId)
             this.router.navigate(['/pedidos']);
             break;
           }
@@ -134,25 +188,67 @@ export class CadstroPedidosComponent implements OnInit {
     } else {
       alert('É necessário preencher todos os campos!');
     }
+
+    // if (this.clienteId && this.listaProdutosId && this.pais && this.estado && this.cidade && this.bairro && this.rua && this.numero && this.cep) {
+    //   console.log("ANTES: " + this.clienteId)
+    //   this.usuarioService.inserirEndereco(this.pais, this.estado, this.cidade, this.bairro, this.rua, this.numero, this.cep)
+    //   this.usuarioService.buscarEndereco().then((resultado: any) => {
+    //     for (let i = 0; i < resultado.length; i++) {
+    //       if (this.numero == resultado[i].NUMERO && this.cep == resultado[i].CEP) {
+    //         console.log("ENDEREÇO ID: ",resultado[i].ID)
+    //         console.log("CLIENTE ID: ", this.clienteId)
+    //         console.log("PRODUTO ID: ", this.listaProdutosId)
+    //         this.enderecoId = resultado[i].ID;
+    //         this.usuarioService.inserirPedido(this.clienteId, this.listaProdutosId, this.enderecoId)
+    //         this.router.navigate(['/pedidos']);
+    //         break;
+    //       }
+    //     }
+    //   });
+
+
+    // } else {
+    //   alert('É necessário preencher todos os campos!');
+    // }
   }
 
   editar() {
+
     if (this.pais && this.estado && this.cidade && this.bairro && this.rua && this.numero && this.cep) {
       
       this.usuarioService.buscarEndereco().then((resultado: any) => {
-        for (let i = 0; i < resultado.length; i++) {
+        
+        for (let i1 = 0; i1 < resultado.length; i1++) {
           let NumeroAntigo = localStorage.getItem('NUMEROANTIGO')
-          if (NumeroAntigo == resultado[i].NUMERO && this.cep == resultado[i].CEP) {
-            this.enderecoId = resultado[i].ID;
+          let CepAntigo = localStorage.getItem('CEPANTIGO')
+          
+          if (NumeroAntigo == resultado[i1].NUMERO && CepAntigo == resultado[i1].CEP) {
+            this.enderecoId = resultado[i1].ID;
+            
             this.usuarioService.editarEndereco(this.pais, this.estado, this.cidade, this.bairro, this.rua, this.numero, this.cep, this.enderecoId)
+
             this.usuarioService.buscarEndereco().then((resultado: any) => {
-              for (let i = 0; i < resultado.length; i++) {
-                if (this.numero == resultado[i].NUMERO && this.cep == resultado[i].CEP) {
-                  this.enderecoId = resultado[i].ID;
-                  this.usuarioService.editarPedido(this.clienteId, this.listaProdutosId, this.enderecoId, this.index).then((resultado: any) => {
-                    this.router.navigate(['/pedidos']);
+              for (let i2 = 0; i2 < resultado.length; i2++) {
+                
+                if (this.numero == resultado[i2].NUMERO && this.cep == resultado[i2].CEP) {
+                  this.enderecoId = resultado[i2].ID;
+                  
+                  this.usuarioService.buscarPedido().then((resultado: any) => {
+                    
+                    for (let i3 = 0; i3 < resultado.length; i3++) {
+                      
+                      if (resultado[i3].ENDERECO_ID == this.enderecoId && resultado[i3].CLIENTE_ID == this.clienteId && resultado[i3].PRODUTO_ID == this.ProdutoId) {
+                        console.log("PASSOU")
+                        let PedidoId = resultado[i3].ID;
+                        this.usuarioService.editarPedido(this.clienteId + 1, this.ProdutoId + 1, this.enderecoId, PedidoId)
+                        this.router.navigate(['/pedidos']);
+                        break;
+                      }
+
+                    }
                   })
-                  break;
+
+
                 }
               }
             });
@@ -163,6 +259,33 @@ export class CadstroPedidosComponent implements OnInit {
     } else {
       alert('É necessário preencher todos os campos!');
     }
+
+    //   if (this.pais && this.estado && this.cidade && this.bairro && this.rua && this.numero && this.cep) {
+
+    //     this.usuarioService.buscarEndereco().then((resultado: any) => {
+    //       for (let i = 0; i < resultado.length; i++) {
+    //         let NumeroAntigo = localStorage.getItem('NUMEROANTIGO')
+    //         if (NumeroAntigo == resultado[i].NUMERO && this.cep == resultado[i].CEP) {
+    //           this.enderecoId = resultado[i].ID;
+    //           this.usuarioService.editarEndereco(this.pais, this.estado, this.cidade, this.bairro, this.rua, this.numero, this.cep, this.enderecoId)
+    //           this.usuarioService.buscarEndereco().then((resultado: any) => {
+    //             for (let i = 0; i < resultado.length; i++) {
+    //               if (this.numero == resultado[i].NUMERO && this.cep == resultado[i].CEP) {
+    //                 this.enderecoId = resultado[i].ID;
+    //                 this.usuarioService.editarPedido(this.clienteId, this.listaProdutosId, this.enderecoId, this.index).then((resultado: any) => {
+    //                   this.router.navigate(['/pedidos']);
+    //                 })
+    //                 break;
+    //               }
+    //             }
+    //           });
+    //         }
+    //       }
+    //     });
+
+    //   } else {
+    //     alert('É necessário preencher todos os campos!');
+    //   }
   }
 
 }
